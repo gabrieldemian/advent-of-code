a106c889ca9e4589c45c5eaaeeb073c9c9326df9
gabrieldemian <gabrielgcr45@gmail.com>
2024-07-16 22:34:59 +0300

    2022/6

---
+++ 2022/day-06/src/bin/1.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let input = include_str!("../../input.txt");
+    let output = find_marker(input);
+    dbg!(output);
+}
+
+// rearrange crates
+fn find_marker(input: &str) -> usize {
+    let mut r = 0;
+    'outer: for i in 0..input.len() {
+        let Some(marker) = input.get(i..i + 4) else { break };
+        // println!("{marker:?}");
+        for (y, c) in marker.chars().enumerate() {
+            if marker
+                .chars()
+                .enumerate()
+                .find(|v| v.1 == c && v.0 != y)
+                .is_some()
+            {
+                continue 'outer;
+            }
+        }
+        // println!("found: {marker:?}");
+        r = i + 4;
+        break;
+    }
+    r
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_01() {
+        let output = find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        assert_eq!(output, 7);
+    }
+}
